95b5fbaa31c05b4ddeaf667d818492a30bfd62c1
jo-wil <joey.wilson.a@gmail.com>
2015-05-12 15:01:02 -0700

    working on admin stuff

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ class DB:
         conn.commit()
         conn.close()
       
-    def query(self,class_name,topic):
+    def query(self, class_name, topic):
         conn = sqlite3.connect('application.db')
         c = conn.cursor()  
         c.execute("SELECT problem, solution FROM Problems WHERE class = ? AND topic = ? ORDER BY level", (class_name,topic))
@@ -21,53 +21,61 @@ class DB:
         for row in rows:
            result.append({'problem':row[0],'solution':row[1]})
         return json.dumps(result)
-      
+
+    def insert(self, password, data):
+        if password == 'pass':
+            return self.parse_data(data)
+        return 'Incorrect Password!'
+
     def parse_data(self, data):
-        data_rows = data.split('\n')
+        data_rows = data.split('\n')[:-1]
       
         # data validation and normalization
         index = 0
         valid_rows = []
         for row in data_rows:
-            row = row.split(',')
-            if len(row) == 5: # to get rid of the last line
-                problem = row[0]
-                problem = problem.strip()
+            row = row.split('\t')
+            if len(row) != 5:
+               return 'Line ' + str(index + 1) + ' has ' + str(len(row)) + ' columns, should have 5'
+            problem = row[0]
+            problem = problem.strip()
             
-                solution = row[1]
-                solution = solution.strip()
+            solution = row[1]
+            solution = solution.strip()
             
-                class_num = row[2]
-                class_num = class_num.strip()
-                try:
-                    class_num = int(class_num)
-                except:
-                    return 'Error parsing class num for row ' + str(index)
-                   
-                topic = row[3]
-                topic = topic.strip()
-                topic = topic.lower()
-                topic = topic.replace(' ','');
+            class_num = row[2]
+            class_num = class_num.strip()
+            try:
+                class_num = int(class_num)
+            except:
+                return 'Error parsing class num for row ' + str(index + 1)
+                  
+            topic = row[3]
+            topic = topic.strip()
+            topic = topic.lower()
+            topic = topic.replace(' ','');
                 
-                level = row[4]
-                level.strip()
-                try:
-                    level = int(level)
-                except:
-                    return 'Error parsing level for row ' + str(index)
+            level = row[4]
+            level.strip()
+            try:
+                level = int(level)
+            except:
+                return 'Error parsing level for row ' + str(index + 1)
                 
-                valid_rows.append([problem, solution, class_num, topic, level])
-                index += 1
+            valid_rows.append([problem, solution, class_num, topic, level])
+            index += 1
       
         # database access
         conn = sqlite3.connect('application.db')
         c = conn.cursor()  
         
+        index = 0
         for row in valid_rows:
             try:
                 c.execute("INSERT INTO Problems VALUES(?,?,?,?,?)", (row[0],row[1],row[2],row[3],row[4]))
             except sqlite3.Error, e:
-                return 'Error %s:' % e.args[0]
+                return 'Error: ' + e.args[0] + ', on line ' + str(index + 1)
+            index += 1
         
         conn.commit()
         conn.close()
--- server.py
+++ server.py
@@ -11,6 +11,7 @@ render = web.template.render('templates/')
 urls = (
     '/worksheet', 'Worksheet',
     '/database', 'Database',
+    '/admin', 'Admin',
     '/', 'Index'
 )
 
@@ -27,6 +28,12 @@ class Database:
     def GET(self):
         user_data = web.input(class_name = 'None', topic = 'None')
         return db.query(user_data.class_name, user_data.topic)
+    def POST(self):
+        user_data = web.input(password = 'None', data = 'None')
+        return db.insert(user_data.password, user_data.data)
+class Admin:
+    def GET(self):
+        return render.admin()
 
 class Index:        
     def GET(self):
